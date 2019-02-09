b0e16b27235e08dca6ec52068807a43185766d62
Mani Jafari <manielli@gmail.com>
2019-02-08 19:48:55 -0800

    Created gym_classes controller and views for new gym_class and index of gym_classes and gym_classes show, added their links on the top of page header

---
+++ app/controllers/gym_classes_controller.rb
@@ -0,0 +1,34 @@
+class GymClassesController < ApplicationController
+    before_action :find_gym_class, only: [:show]
+    def new
+        @gym_class = GymClass.new
+    end
+
+    def create
+        @gym_class = GymClass.new gym_class_params
+
+        if @gym_class.save
+            redirect_to gym_glass_path(@gym_class.id)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @gym_classes = GymClass.all.order(created_at: :desc)
+
+        render :index
+    end
+
+    def show
+    end
+
+    private
+    def gym_class_params
+        params.require(:gym_class).permit(:class_type, :description, :cost, :maximum_clients)
+    end
+
+    def find_gym_class
+        @gym_class = GymClass.find params[:id]
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+GymClass.destroy_all
+
+10.times do
+    created_at = Faker::Date.backward(365 * 2)
+
+    gc = GymClass.create(
+        class_type: ["MuayHIIT", "MuayFit", "MuayThai", "Kickboxing", "Core Builder"].sample,
+        maximum_clients: rand(1..15),
+        description: Faker::Lorem.sentences(1, true),
+        cost: ["20","25","15"].sample,
+        created_at: created_at,
+        updated_at: created_at
+    )
+end
