ecebdcb7c3f1003494225d866666db193b3b3b1c
Mani Jafari <manielli@gmail.com>
2019-02-17 20:57:24 -0800

    Added users controller show and index actions with views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-    
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:new, :create]
+    before_action :coach_authorize_user!, except: [:new, :create, :index]
+    before_action :client_authorize_user!, only: [:show, :edit, :update, :new, :create, :index, :destroy]
+
+
     def new
         @user = User.new
+    end
+
+    def show
+    end
+
+    def index
+        @users = User.all.order(first_name: :asc)
     end
 
     def create
@@ -19,5 +31,23 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :date_of_birth)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def coach_authorize_user!
+        unless can?(:crud, @user)
+            flash[:danger] = "Access Denied!"
+            redirect_to users_path
+        end
+    end
+
+    def client_authorize_user!
+        unless can?(:u, @user)
+            flash[:danger] = "You are not authorized to make these changes"
+            redirect_to users_path
+        end
     end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,6 +30,7 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
     alias_action(:create, :edit, :update, :delete, to: :crud)
+    alias_action(:edit, :update, to: :u)
 
     can(:crud, GymClass) do |gym_class|
       user.role == "Coach"
@@ -45,6 +46,14 @@ class Ability
 
     can(:book, Occurence) do |occurence|
       user.persisted? && occurence.user != user
+    end
+
+    can(:crud, User) do |user|
+      user.role == "Coach"
+    end
+
+    can(:u, User) do |user|
+      user.role == "Client"
     end
   end
 end
