4b4b4fc9ac20ea655ecb9d73098d5b1ed6599aec
Mani Jafari <manielli@gmail.com>
2020-04-07 16:12:22 -0700

    Add api users controller for create, show, index, update, destroy to routes, add status code for api json responses

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < Api::ApplicationController
 
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
-            render json: { id: user.id }
+            render json: { id: user.id, status: 200 }, status: 200
         else
             render json: { status: 404 }, status: 404
         end
